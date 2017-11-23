65aec9e21aa925b8c122a16fe3556e3028ef2384
Eitan Sternlicht <eitanoffun@gmail.com>
2017-11-23 08:32:11 +0200

    wip

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "ex2.h"
-#include "string.h"
+#include <string.h>
 #define N 30
 
 
-
-void print_list(void* list, void (*print_item)(void* item), void* (*get_next)(void* item)) {
-
-    for (void* current = list; current != NULL; current = get_next(current)) {
-        print_item(current);
-        printf("\n");
-    }
-}
+// TODO free malloc, asset malloc worked
 
 Stud newStudent(int id) {
     return (Stud)
@@ -24,81 +18,205 @@ Stud newStudent(int id) {
             , .incomesList = NULL
             };
 }
-void newIncome(int id, char* date, double amount) {
-
-}
-void wantedStudent(int id) {
-
-}
-void printArray() {
-
-}
-// part B
-void sortArray(enum Ordering (*compare)(const Stud a, const Stud b), enum SortBy sb, enum SortDirection sd) {
-}
-int sortArray_(enum Ordering (*cmp)(const Stud s1, const Stud s2)) {
-
-}
-
-void addIncome(Stud stud, struct Income income) {
-
-}
-void* getNextGrade(void* gradeNode) {
-    struct GradeNode* gn = (struct GradeNode*) gradeNode;
-    return (void*)(gn->next);
-}
 
 
-void printGrade(void* gradeNode) {
-    struct GradeNode* gn = (struct GradeNode*)gradeNode;
-    printf("%s %d", gn->data.courseName, gn->data.grade);
+//
+//void* getNextGradeNode(void* node) {
+//    struct GradeNode* n = (struct GradeNode*) node;
+//    return (void*)(n->next);
+//}
+//void printGradeNode(void *gradeNode) {
+//    struct GradeNode* gn = (struct GradeNode*)gradeNode;
+//    printf("%s %d", gn->data.courseName, gn->data.grade);
+//}
+void printStudent(Stud *stud) {
+    printf("id: %d\n", stud->id);
+    print_list(stud->gradesList, printGradeNode, getNextGradeNode);
 }
-void printStudent(Stud* stud) {
-    printf("%d\n", (*stud).id);
-    print_list((*stud).gradesList, printGrade, getNextGrade);
-}
-void addGrade(struct GradeNode** gradesList, struct Grade* grade) {
-    if (*gradesList == NULL) {
-        // gradesList is empty
-//        *gradesList = malloc(sizeof(struct GradeNode));
-//        struct GradeNode* n = **gradesList;
-        (*gradesList) = malloc(sizeof(**gradesList));
-        (*gradesList)->data = *grade;
-        (*gradesList)->next = NULL;
-    } else {
-        struct GradeNode* current = *gradesList;
-        while (current->next != NULL)
-            current = current->next;
 
-        current->next = malloc(sizeof(struct GradeNode));
-        current->next->data = *grade;
-        current->next->next = NULL;
+void print_list(void* list, void (*print_item)(void* item), void* (*get_next)(void* item)) {
+    void* current = list;
+    if (current == NULL)
+        return;
+    print_item(current);
+    current = get_next(current);
+
+    while (current != NULL) {
+        printf(", ");
+        print_item(current);
+        current = get_next(current);
     }
 }
-int main() {
 
+
+struct IncomeNode* createIncomeNode(Income* income) {
+    struct IncomeNode* nd =
+            (struct IncomeNode*)malloc(sizeof(struct IncomeNode));
+    assert(nd != NULL);
+
+    nd->income = income;
+    nd->next = NULL;
+    nd->prev = NULL;
+
+    return(nd);
+}
+struct GradeNode* createGradeNode(Grade grade) {
+    struct GradeNode* nd =
+            (struct GradeNode*)malloc(sizeof(struct GradeNode));
+    assert(nd != NULL);
+
+
+    (*nd).grade = grade;
+    nd->next = NULL;
+    nd->prev = NULL;
+
+    return (nd);
+}
+void addGrade(void **gradesList, Grade data) {
+    struct GradeNode *node = createGradeNode(data);
+    struct GradeNode *head = *gradesList;
+    if (head == NULL) {
+        *gradesList = node;
+    }
+    else if (head->grade.grade > node->grade.grade){
+        node->next = *gradesList;
+        *gradesList = node;
+        node->prev = *gradesList;
+        node->next->prev = node;
+    } else {
+        struct GradeNode* current = head;
+        while (current->next != NULL && current->next->grade.grade < node->grade.grade) {
+            current = current->next;
+        }
+        node->next = current->next;
+        node->prev = current;
+        if (node->next != NULL)
+            node->next->prev = node;
+        current->next = node;
+    }
+}
+void printGradeNode(void *gradeNode) {
+    struct GradeNode* gn = gradeNode;
+    Grade g = gn->grade;
+    printf("%s %d", g.courseName, g.grade);
+}
+
+void* getNextGradeNode(void* node) {
+    return ((struct GradeNode*)node)->next;
+}
+
+void test() {
     Stud stud1 = newStudent(1234);
+    Grade stud1gradeInA = {"testA", 44};
+    Grade stud1gradeInB = {"testB", 88};
+    Grade stud1gradeInC = {"testC", 66};
 
 
-//    char s[5] = "test";
-//    struct Grade stud1gradeInC;
-//    strcpy(stud1gradeInC.courseName, s);
-//    stud1gradeInC.grade = 90;
+    addGrade((void **) &(stud1.gradesList), stud1gradeInC);
+    addGrade((void **) &(stud1.gradesList), stud1gradeInB);
+    addGrade((void **) &(stud1.gradesList), stud1gradeInA);
 
-    struct Grade stud1gradeInD = {"test2", 99};
-    struct Grade stud1gradeInC = {"test3", 59};
-
-//    struct GradeNode gn = {.data = stud1gradeInC, .next = NULL};
-//    struct GradeNode gn2 = {.data = stud1gradeInD, .next = &gn};
-
-    addGrade(&(stud1.gradesList), &stud1gradeInD);
-    addGrade(&(stud1.gradesList), &stud1gradeInC);
+    print_list(stud1.gradesList, printGradeNode, getNextGradeNode);
+}
+/**
+ * returns next available index
+ * @return if student exists, or if array is full - returns -1
+ */
+int getNextAvailableIndex(Stud students[], int size, int id) {
 
 
+}
+/**
+ *
+ * @param students
+ * @param currentIndex
+ * @param id
+ * @return 1 if success, 0 if failed
+ */
+int addStudent (Stud students[], int currentIndex, unsigned int id) {
+    for (int i = 0; i < currentIndex; ++i) {
+        if (students[i].id == id) {
+            printf("DOUBLESTUD");
+            return 0;
+        }
+    }
+    students[currentIndex] = newStudent(id);
+    return 1;
+}
 
-//    stud1.gradesList = &gn2;
-    printStudent(&stud1);
-//    printGrade(&gn2);
-//    print_list(&gn2, printGrade, getNextGrade);
+Stud* getStudent(Stud students[], int id, int currentIndex) {
+    for (int i = 0; i < currentIndex; ++i) {
+        if (students[i].id == id)
+            return &students[i];
+    }
+    return NULL;
+}
+
+void printAllStudents(Stud students[], int currentIndex) {
+    if (currentIndex == 0)
+        return;
+    printStudent(&students[0]);
+    for (int i = 1; i < currentIndex; ++i) {
+        printf("\n");
+        printStudent(&students[i]);
+    }
+}
+
+int main() {
+    Stud students[N];
+    int currentIndex = 0;
+
+    char op[3];
+
+    scanf("%s", op);
+
+    while (strcmp(op, "end") != 0) {
+        if (strcmp(op, "ns") == 0) {
+            unsigned int id;
+            scanf("%u", &id);
+            Stud s = newStudent(id);
+            int success = addStudent(students, currentIndex, id);
+            if (success)
+                currentIndex++;
+        } else if (strcmp(op, "ng") == 0) {
+            unsigned int id;
+            char courseName[COURSE_NAME_MAX_LENGTH];
+            unsigned int grade;
+            scanf("%d", &id);
+            scanf("%s", courseName);
+            scanf("%d", &grade);
+    
+            Stud* stud = getStudent(students, id, currentIndex);
+            if (stud == NULL) {
+                printf("NA");
+                return 0;
+            }
+
+            Grade newGrade;
+            strcpy(newGrade.courseName, courseName);
+            newGrade.grade = grade;
+            addGrade((void **) &(stud->gradesList), newGrade);
+
+        } else if (strcmp(op, "ni") == 0) {
+        } else if (strcmp(op, "ws") == 0) {
+            unsigned int id;
+            scanf("%d", &id);
+            Stud* stud = getStudent(students, id, currentIndex);
+            if (stud != NULL) {
+                printStudent(stud);
+            } else {
+                printf("NA");
+                return 0;
+            }
+
+        } else if (strcmp(op, "pa") == 0) {
+            printAllStudents(students, currentIndex);
+        } else if (strcmp(op, "sa") == 0) {
+
+        }
+        scanf("%s", op);
+    }
+
+
     return 0;
 }
--- ex2/ex2.h
+++ ex2/ex2.h
@@ -2,40 +2,42 @@
 #define WORK_NAME_MAX_LENGTH 20
 
 
+
 typedef struct
 {
-    int id;
+    unsigned int id;
     float averageOfGrades;
     float averageOfInserts;
+    unsigned int gradeCount;
+    unsigned int incomeCount;
     struct GradeNode* gradesList;
     struct IncomeNode* incomesList;
-
 } Stud;
 
-struct Grade
+typedef struct
 {
     char courseName[COURSE_NAME_MAX_LENGTH];
     unsigned int grade;
+} Grade;
+typedef struct
+{
+    double amount;
+    char workName[WORK_NAME_MAX_LENGTH];
+} Income;
+
+struct IncomeNode
+{
+    Income* income;
+    struct IncomeNode* next;
+    struct IncomeNode* prev;
 };
 
 struct GradeNode
 {
-    struct Grade data;
+    Grade grade;
     struct GradeNode* next;
+    struct GradeNode* prev;
 };
-
-struct Income
-{
-    double amount;
-    char workName[WORK_NAME_MAX_LENGTH];
-};
-
-struct IncomeNode
-{
-    struct Income data;
-    struct IncomeNode* next;
-};
-
 enum Ordering {
     GT,
     LT,
@@ -49,3 +51,16 @@ enum SortBy {
     ID,
     AverageOfGrades
 };
+
+void newIncome(int id, char* date, double amount);
+void wantedStudent(int id);
+void printArray();
+// part B
+void sortArray(enum Ordering (*compare)(const Stud a, const Stud b), enum SortBy sb, enum SortDirection sd);
+int sortArray_(enum Ordering (*cmp)(const Stud s1, const Stud s2));
+void addIncome(Stud stud, Income income);
+struct Node* createNode(void *data);
+void print_list(void* list, void (*print_item)(void* item), void* (*get_next)(void* item));
+void printGradeNode(void *gradeNode);
+void* getNextGradeNode(void* node);
+void addGrade(void **gradesList, Grade data);
